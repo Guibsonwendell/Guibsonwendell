08b2d8126eeeeb888e6b5a2bf2a41e868c259349
Guibson Wendell Negreiros Pimentel <01653470@sempreuninassau.com.br>
2023-08-31 21:16:54 -0300

    Anexando 30.08

---
+++ index.js
@@ -0,0 +1,25 @@
+const a = 3
+const b = 4
+const resultadoSoma = a + b
+
+console.log ("A soma é", resultadoSoma)
+
+const c = 3
+const d = 5
+const resultmultdiv = (c * d) / 2
+
+console.log ("O resultado da multiplicação dividido por 2 é", resultmultdiv)
+
+const e = 5
+const f = 4
+const subtracao = (f - e) * -1
+
+console.log("Resultado da subtração X -1", subtracao)
+
+const g = 234
+const h = 5
+const div = g / h
+const divisao = g % h
+
+
+console.log ("O resultado é", div , "o resto é", divisao)
